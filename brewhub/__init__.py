cf36b8050388ee87ad0d5ba8ba82d0824036a0bb
Syrnik Konrad <syrnikkonrad@gmail.com>
2022-05-22 23:52:05 +0200

    Add some new .html files

--- brewhub/__init__.py
+++ brewhub/__init__.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "7&Fa2sa23j"
+    app.debug = True
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
+
+    @app.errorhandler(500)
+    def page_not_found(e):
+        return render_template('500.html'), 500
 
     from .views import views
 
     app.register_blueprint(views, url_prefix='/')
-
 
     return app
--- brewhub/views.py
+++ brewhub/views.py
@@ -2,10 +2,17 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/')
 def home():
     return render_template('index.html')
 
+
 @views.route('/test')
 def test():
     return render_template('test.html')
+
+
+@views.route('/login')
+def login():
+    return render_template('login.html')
