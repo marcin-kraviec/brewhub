6a7ab6fcbeba9fe8152275e6e3d81b5889f4a791
Syrnik Konrad <syrnikkonrad@gmail.com>
2022-05-29 22:29:55 +0200

    Fix beer styles filtration

--- brewhub/views.py
+++ brewhub/views.py
@@ -126,35 +126,69 @@ def register():
 
 @views.route('/beer_styles', methods=['GET', 'POST'])
 def beer_styles():
+    american_light_lager = {'balance': 'balanced', 'fermentation': 'bottom-fermented', 'lager': 'lagered', 'country': 'north-america', 'color': 'pale-color', 'family': 'pale-lager-family', 'strength': 'session-strength', 'style': 'traditional-style'}
+    american_lager = {'balance': 'balanced', 'fermentation': 'bottom-fermented', 'lager': 'lagered', 'country': 'north-america', 'color': 'pale-color', 'family': 'pale-lager-family', 'strength': 'standard-strength', 'style': 'traditional-style'}
+    cream_ale = {'balance': 'balanced', 'fermentation': 'any-fermentation', 'country': 'north-america', 'color': 'pale-color', 'family': 'pale-ale-family', 'strength': 'standard-strength', 'style': 'traditional-style'}
+    american_wheat_beer = {'fermentation': 'any-fermentation', 'balance': 'balanced', 'style': 'craft-style', 'country': 'north-america', 'color': 'pale-color', 'strength': 'standard-strength', 'family': 'wheat-beer-family'}
+    international_pale_lager = {'balance': 'balanced', 'fermentation': 'bottom-fermented', 'lager': 'lagered', 'color': 'pale-color', 'family': 'pale-lager-family', 'strength': 'standard-strength', 'style': 'traditional-style'}
+    international_amber_lager = {'color': 'amber-color', 'family': 'amber-lager-family', 'fermentation': 'bottom-fermented', 'lager': 'lagered', 'malty': 'malty', 'strength': 'standard-strength', 'style': 'traditional-style'}
 
-    american_light_lager = ['balanced', 'bottom-fermented', 'lagered', 'north-america', 'pale-color', 'pale-lager-family', 'session-strength', 'traditional-style']
-    american_lager = ['balanced', 'bottom-fermented', 'lagered', 'north-america', 'pale-color', 'pale-lager-family', 'standard-strength', 'traditional-style']
-    cream_ale = ['balanced', 'any-fermentation', 'north-america', 'pale-color', 'pale-ale-family', 'standard-strength', 'traditional-style']
-    american_wheat_beer = ['any-fermentation', 'balanced', 'craft-style', 'north-america', 'pale-color', 'standard-strength', 'wheat-beer-family']
-    international_pale_lager = ['balanced', 'bottom-fermented', 'lagered', 'pale-color', 'pale-lager-family', 'standard-strength', 'traditional-style']
-    international_amber_lager = ['amber-color', 'amber-lager-family', 'bottom-fermented', 'lagered', 'malty', 'standard-strength', 'traditional-style']
+    # put each beer style to one list
+    styles = [american_light_lager, american_lager, cream_ale, american_wheat_beer, international_pale_lager, international_amber_lager]
 
-    filters = request.form.getlist('filter')
+    # get sectional parameters from html file
+    balance = request.form.getlist('balance')
+    fermentation = request.form.getlist('fermentation')
+    lager = request.form.getlist('lager')
+    malty = request.form.getlist('malty')
+    country = request.form.getlist('country')
+    color = request.form.getlist('color')
+    family = request.form.getlist('family')
+    strength = request.form.getlist('strength')
+    style = request.form.getlist('style')
 
-    check_american_light_lager = any(item in american_light_lager for item in filters)
-    check_american_lager = any(item in american_lager for item in filters)
-    check_cream_ale = any(item in cream_ale for item in filters)
-    check_american_wheat_beer = any(item in american_wheat_beer for item in filters)
-    check_international_pale_lager = any(item in international_pale_lager for item in filters)
-    check_international_amber_lager = any(item in international_amber_lager for item in filters)
+    # put all parameters into one dictionary
+    filters = {'balance': balance, 'fermentation': fermentation, 'lager': lager, 'malty': malty, 'country': country, 'color': color, 'family': family, 'strength': strength, 'style': style}
 
-    if not filters:
-        check_american_light_lager = True
-        check_american_lager = True
-        check_cream_ale = True
-        check_american_wheat_beer = True
-        check_international_pale_lager = True
-        check_international_amber_lager = True
+    check_american_light_lager = False
+    check_american_lager = False
+    check_cream_ale = False
+    check_american_wheat_beer = False
+    check_international_pale_lager = False
+    check_international_amber_lager = False
 
-    print(filters)
-    print(check_american_light_lager)
-    return render_template('beer_styles.html', check_american_light_lager=check_american_light_lager, check_american_lager=check_american_lager, check_cream_ale=check_cream_ale, check_american_wheat_beer=check_american_wheat_beer,
-                           check_international_pale_lager=check_international_pale_lager, check_international_amber_lager=check_international_amber_lager)
+    checks = [check_american_light_lager, check_american_lager, check_cream_ale, check_american_wheat_beer, check_international_pale_lager, check_international_amber_lager]
+
+    for i in range(len(styles)):
+        if (styles[i].get('balance') in filters.get('balance') or filters.get('balance') == []) \
+                and (styles[i].get('fermentation') in filters.get('fermentation') or filters.get('fermentation') == []) \
+                and (styles[i].get('lager') in filters.get('lager') or filters.get('lager') == []) \
+                and (styles[i].get('malty') in filters.get('malty') or filters.get('malty') == []) \
+                and (styles[i].get('country') in filters.get('country') or filters.get('country') == []) \
+                and (styles[i].get('color') in filters.get('color') or filters.get('color') == []) \
+                and (styles[i].get('family') in filters.get('family') or filters.get('family') == []) \
+                and (styles[i].get('strength') in filters.get('strength') or filters.get('strength') == []) \
+                and (styles[i].get('style') in filters.get('style') or filters.get('style') == []):
+            checks[i] = True
+
+    # check_american_light_lager = any(item in american_light_lager for item in filters)
+    # check_american_lager = any(item in american_lager for item in filters)
+    # check_cream_ale = any(item in cream_ale for item in filters)
+    # check_american_wheat_beer = any(item in american_wheat_beer for item in filters)
+    # check_international_pale_lager = any(item in international_pale_lager for item in filters)
+    # check_international_amber_lager = any(item in international_amber_lager for item in filters)
+
+    # if not filters:
+    #     check_american_light_lager = True
+    #     check_american_lager = True
+    #     check_cream_ale = True
+    #     check_american_wheat_beer = True
+    #     check_international_pale_lager = True
+    #     check_international_amber_lager = True
+
+    # return render_template('beer_styles.html', check_american_light_lager=check_american_light_lager)
+
+    return render_template('beer_styles.html', checks=checks)
 
 
 @views.route('/beer_styles/<string:s>', methods=['GET', 'POST'])
