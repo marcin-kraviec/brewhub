88f701f003184e527381d0ecc859dfda8989f990
Marcin Krawiec <marcin.krawiec@protonmail.com>
2022-07-04 22:18:46 +0200

    Fix recipe edit

--- brewhub/database_connector.py
+++ brewhub/database_connector.py
@@ -10,7 +10,7 @@ class DatabaseConnector:
         database = mysql.connector.connect(host=HOST, user=USER, password=PASSWORD, database=DATABASE,
                                            auth_plugin='mysql_native_password')
 
-        print('DUPA')
+
     except mysql.connector.Error as e:
         logging.critical('Connection to database has not been established: ' + str(e))
         sys.exit()
